41f4bbc122fa3b8656cc6212d6d3728936859e34
Suraj Patidar <167690910+surajpatidar1@users.noreply.github.com>
2025-09-05 18:46:37 +0530

    page optimization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
 'use client'
 import { useState } from 'react';
 import Dashboard from '../app/dashboard/page'
-import TestForm from './test/page';
+
 export default function Home() {
 
-  const [testing,setTesting] = useState(false)
   return (
     <div className="font-sans ">
-      {
-        testing ?  <TestForm/> : <Dashboard/>
-      }
-      
+      <Dashboard/>
     </div>
   );
 }
