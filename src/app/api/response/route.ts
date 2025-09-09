e986dc152e16f664c359c9fd89b4d1fe98140680
Suraj Patidar <167690910+surajpatidar1@users.noreply.github.com>
2025-09-09 23:36:20 +0530

    undo changes

--- src/app/api/response/route.ts
+++ src/app/api/response/route.ts
@@ -1,9 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { HumanMessage } from "@langchain/core/messages";
 import agent from "../agent/mainAgent";
-import { decideChain } from "./decideFuction";
-import Askchain from "../(models)/model";
-import Datachain from "../(models)/modelForDataFetch";
+
 
 export async function POST(req: NextRequest) {
   try {
@@ -20,29 +18,19 @@ export async function POST(req: NextRequest) {
     
     const user = userName || "Guest";
 
-        const target = decideChain(input);
-    let response;
-    let finalText;
 
-       if (target === "ask") {
-      response = await Askchain.invoke({ input, context: "" });
-      finalText = response.content ?? JSON.stringify(response);
-    } else if (target === "data") {
-      response = await Datachain.invoke({ input });
-      finalText = response.content ?? JSON.stringify(response);
-    } else {
-      response = await agent.invoke(
+    const  response = await agent.invoke(
         { messages: [new HumanMessage(input)] },
         { configurable: { thread_id: sessionId, user_name: user } }
       );
 
 
     const aiMessage = response.messages[response.messages.length - 1];
-     finalText = Array.isArray(aiMessage.content)
+    const finalText = Array.isArray(aiMessage.content)
       ? aiMessage.content.map((c: any) => c?.text || "").join(" ")
       : aiMessage.content;
 
-    }
+    
     return NextResponse.json({
       success: true,
       content: finalText,
